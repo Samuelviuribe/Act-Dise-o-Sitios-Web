9308293fd6ca3de934d243a386a96fd71fcdb3c9
Samuelviuribe <vianasamuel828@gmail.com>
2024-11-08 23:34:58 -0500

    NavBar Agregada y lista para agregar a otras paginas

---
+++ Js/navbar.js
@@ -0,0 +1,30 @@
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/* Menu show */
+if(navToggle){
+   navToggle.addEventListener('click', () =>{
+      navMenu.classList.add('show-menu')
+   })
+}
+
+/* Menu hidden */
+if(navClose){
+   navClose.addEventListener('click', () =>{
+      navMenu.classList.remove('show-menu')
+   })
+}
+
+
+function loadNavbar() {
+    fetch('partials/navbar.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('navbar').innerHTML = data;
+        })
+        .catch(error => console.error('Error al cargar el navbar:', error));
+}
+
+document.addEventListener('DOMContentLoaded', loadNavbar);
